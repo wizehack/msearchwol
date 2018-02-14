af6c0a98b97b78694742ca87c34eade7b9b66753
wizehack <wizehack@gmail.com>
2018-02-14 02:13:42 +0000

    change searchTarget to dial spec

--- src/Main.java
+++ src/Main.java
@@ -37,7 +37,7 @@ public class Main {
 
 
     public static void main(String[] args) {
-        String searchTarget = "urn:iot-hub-lge:service:iot:1";
+        String searchTarget = "urn:dial-multiscreen-org:service:dial:1";
 
         /* Find upnp device */
         UpnpClient upnpc = new UpnpClient(searchTarget);
